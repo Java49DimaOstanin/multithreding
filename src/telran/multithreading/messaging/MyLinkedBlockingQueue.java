5f4e43de5065b6c6c239a8abb259d03e06941880
DimaOstanin <xnadab@gmail.com>
2023-10-15 15:04:33 +0300

    HW #49 MyBlockingQueue

--- src/telran/multithreading/messaging/MyLinkedBlockingQueue.java
+++ src/telran/multithreading/messaging/MyLinkedBlockingQueue.java
@@ -1,9 +1,17 @@
 package telran.multithreading.messaging;
 
+import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 	int limit;
+	private LinkedList<E> queue = new LinkedList<>();
+	private Lock monitor = new ReentrantLock();
+	private Condition consumerWaitingCondition = monitor.newCondition();
+	private Condition producerWaitingCondition = monitor.newCondition();
 	
 	public MyLinkedBlockingQueue(int limit) {
 		super();
@@ -12,62 +20,153 @@ public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
 	
 	@Override
 	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			monitor.lock();
+			if(queue.size() == limit) {
+				throw new IllegalStateException();
+			}
+			boolean res = queue.add(e);
+			consumerWaitingCondition.signal();
+			return res;
+		}finally {
+			monitor.unlock();
+		}
+		
 	}
 
 	@Override
 	public boolean offer(E e) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = true;
+		try {
+			monitor.lock();
+			if(queue.size() == limit) {
+				res = false;
+				
+			}else {
+				queue.add(e);
+				consumerWaitingCondition.signal();
+			}
+			return res;
+		} finally {
+			monitor.unlock();
+		}
+		
 	}
 
 	@Override
-	public void put(E e) {
-		// TODO Auto-generated method stub
+	public void put(E e) throws InterruptedException{
+		try {
+			monitor.lock();
+			while(queue.size() == limit) {
+				producerWaitingCondition.await();
+			}
+			queue.add(e);
+			consumerWaitingCondition.signal();
+		}finally {
+			monitor.unlock();
+		}
 		
 	}
 
 	@Override
 	public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			monitor.lock();
+			while(queue.size() == limit) {
+				if(!producerWaitingCondition.await(timeout, unit)) {
+						return false;
+				}
+			}
+			queue.add(e);
+			consumerWaitingCondition.signal();
+			return true;
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	@Override
 	public E take() throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			monitor.lock();
+			while(queue.isEmpty()) {
+				consumerWaitingCondition.await();
+			}
+			E res = queue.remove(0);
+			producerWaitingCondition.signal();
+			return res;
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	@Override
 	public E poll(long timeout, TimeUnit unit) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			monitor.lock();
+			while(queue.isEmpty()) {
+				if(!consumerWaitingCondition.await(timeout, unit)) {
+					return null;
+				}
+			}
+			E res = queue.remove();
+			producerWaitingCondition.signal();
+			return res;
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	@Override
 	public E remove() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			monitor.lock();
+			E result = queue.remove();
+			producerWaitingCondition.signal();
+			return result;
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+		E result = null;
+		try {
+			monitor.lock();
+			if(queue.size() != 0) {
+				result = queue.get(0);
+			}
+			return result;
+		} finally {
+			monitor.unlock();
+		}
+
 	}
 
 	@Override
 	public E element() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			monitor.lock();
+			return queue.element();
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	@Override
 	public E poll() {
-		// TODO Auto-generated method stub
-		return null;
+		E result = null;
+		try {
+			monitor.lock();
+			result = queue.poll();
+			if(result != null) {
+				producerWaitingCondition.signal();
+			}
+			return result;
+		} finally {
+			monitor.unlock();
+		}
 	}
 
 	
